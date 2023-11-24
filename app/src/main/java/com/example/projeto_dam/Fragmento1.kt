b12cfdf899c51ade584d715ee0b2fc15751b2ab0
Hugo1506 <128063565+Hugo1506@users.noreply.github.com>
2023-11-24 15:36:59 +0000

    botão no fragmento 1

--- app/src/main/java/com/example/projeto_dam/Fragmento1.kt
+++ app/src/main/java/com/example/projeto_dam/Fragmento1.kt
@@ -1,10 +1,17 @@
 package com.example.projeto_dam
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,11 +28,15 @@ class Fragmento1 : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    lateinit var button: Button
+    lateinit var imageView: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
+
         }
     }
 
@@ -33,6 +44,7 @@ class Fragmento1 : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_fragmento1, container, false)
     }
@@ -55,5 +67,6 @@ class Fragmento1 : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+
     }
 }
