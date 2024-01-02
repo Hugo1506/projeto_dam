92748bd062add05787c12eb2f6feebe095f9b2dc
hugo1506 <aluno24171@ipt.pt>
2024-01-02 17:05:56 +0000

    debugging do login

--- app/src/main/java/com/example/projeto_dam/MainActivity.kt
+++ app/src/main/java/com/example/projeto_dam/MainActivity.kt
@@ -1,10 +1,10 @@
-package  com.example.projeto_dam
+package com.example.projeto_dam
 
 import android.os.Bundle
 import android.util.Log
 import android.view.KeyEvent
-import android.view.KeyEvent.keyCodeFromString
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.Toast
@@ -15,21 +15,22 @@ import com.google.android.material.tabs.TabLayout
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     lateinit var tabLayout: TabLayout
     lateinit var viewPager2: ViewPager2
     private lateinit var myViewPagerAdapter: MyViewPagerAdapter
-
+    private var dados: List<DadosAuth> = ArrayList()
+    private var auth: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var userNameText = findViewById<EditText>(R.id.Username)
-        var passwordText = findViewById<EditText>(R.id.Password)
-        var dados: List<DadosAuth> = ArrayList()
+
+        val userNameText = findViewById<EditText>(R.id.Username)
+        val passwordText = findViewById<EditText>(R.id.Password)
         val imm = getSystemService<InputMethodManager>()
-        var auth: Boolean = false
 
         userNameText.requestFocus()
         imm?.showSoftInput(userNameText, InputMethodManager.SHOW_IMPLICIT)
@@ -50,9 +51,7 @@ class MainActivity : AppCompatActivity() {
 
             override fun onTabUnselected(tab: TabLayout.Tab?) {}
             override fun onTabReselected(tab: TabLayout.Tab?) {}
-
         })
-
 
         viewPager2.registerOnPageChangeCallback(object :
             ViewPager2.OnPageChangeCallback() {
@@ -61,71 +60,62 @@ class MainActivity : AppCompatActivity() {
                 tabLayout.getTabAt(position)?.select()
             }
         })
+
         tabLayout.visibility = View.INVISIBLE
         viewPager2.visibility = View.INVISIBLE
-        passwordText.setOnEditorActionListener { _, keyCode, k: KeyEvent ->
 
-            @Override
-            fun onEditorAction(v: EditText, actionId: Int, event: KeyEvent): Boolean {
-                if (actionId == keyCodeFromString("IME_ACTION_DONE")) {
-                    val call = RetrofitInitializer().dadosService()?.list()
-                    call?.enqueue(object : Callback<List<DadosAuth>?> {
-                        override fun onResponse(
-                            call: Call<List<DadosAuth>?>,
-                            response: Response<List<DadosAuth>?>
-                        ) {
-                            response.body()?.let {
-                                dados = it
-
-                            }
+        passwordText.setOnEditorActionListener { _, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                val call = RetrofitInitializer().dadosService()?.list()
+                call?.enqueue(object : Callback<List<DadosAuth>?> {
+                    override fun onResponse(
+                        call: Call<List<DadosAuth>?>,
+                        response: Response<List<DadosAuth>?>
+                    ) {
+                        response.body()?.let {
+                            dados = it
                         }
-
-                        override fun onFailure(call: Call<List<DadosAuth>?>, t: Throwable) {
-                            t.message?.let {
-                                Log.e("onFailure error", it)
-
-                            }
-                        }
-                    })
-                    imm?.showSoftInput(passwordText, InputMethodManager.HIDE_IMPLICIT_ONLY)
-                    for (i in dados.indices) {
-                        if (userNameText.text.toString() == dados[i].Username && passwordText.text.toString() == dados[i].Password) {
-
-                            auth = true
-                            break
-                        }
-
                     }
-                    if (auth) {
-                        userNameText.visibility = View.INVISIBLE
-                        passwordText.visibility = View.INVISIBLE
-                        passwordText.clearFocus()
-                        tabLayout.visibility = View.VISIBLE
-                        viewPager2.visibility = View.VISIBLE
-                        Toast.makeText(
-                            this.applicationContext,
-                            "Acesso disponibilizado",
-                            Toast.LENGTH_SHORT
-                        ).show()
+
+                    override fun onFailure(call: Call<List<DadosAuth>?>, t: Throwable) {
+                        t.message?.let {
+                            Log.e("onFailure error", it)
+                        }
                     }
-                    return true
+                })
+
+                imm?.hideSoftInputFromWindow(passwordText.windowToken, 0)
+
+                for (i in dados.indices) {
+                    Log.d(userNameText.text.toString(),"")
+                    Log.d(passwordText.text.toString(),"")
+                    if (userNameText.text.toString() == dados[i].Username &&
+                        passwordText.text.toString() == dados[i].Password
+                    ) {
+                        auth = true
+                        break
+                    }
                 }
 
+                if (auth) {
+                    userNameText.visibility = View.INVISIBLE
+                    passwordText.visibility = View.INVISIBLE
+                    passwordText.clearFocus()
+                    tabLayout.visibility = View.VISIBLE
+                    viewPager2.visibility = View.VISIBLE
+                    Toast.makeText(
+                        this.applicationContext,
+                        "Acesso disponibilizado",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    Toast.makeText(this.applicationContext, "Acesso Negado", Toast.LENGTH_LONG).show()
+                }
 
-                return false
-            }
-            if(onEditorAction(passwordText, keyCode, k ) == true ) {
-                return@setOnEditorActionListener true
+                true
             } else {
-                Toast.makeText(this.applicationContext, "Acesso Negado", Toast.LENGTH_LONG).show()
-                return@setOnEditorActionListener false
+                false
             }
-
         }
-
     }
-
-
-
-
 }
