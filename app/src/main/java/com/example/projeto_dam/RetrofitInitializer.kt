55ffdae3df90b7402195139b142bf21e5dd3ef1d
Luis Pedro Polastrini Baptista Torres <malagothick@gmail.com>
2024-01-10 17:21:52 +0000

    Suspender main thread com call.await() e por processo de autenticação atrás de setOnEditorActionListener

--- app/src/main/java/com/example/projeto_dam/RetrofitInitializer.kt
+++ app/src/main/java/com/example/projeto_dam/RetrofitInitializer.kt
@@ -1,14 +1,16 @@
 package com.example.projeto_dam
 
-import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitInitializer {
-    private var gson: Gson = GsonBuilder().setLenient().create()
+    private var okHTTP = OkHttpClient.Builder().build()
+    private var gson = GsonBuilder().setLenient().create()
     private var retrofit = Retrofit.Builder()
         .baseUrl("https://api.sheety.co/f78f66f4c4513d698d0e13096825199b/vinx/")
+        .client(okHTTP)
         .addConverterFactory(GsonConverterFactory.create(gson))
         .build()
     fun dadosService(): Auth? = retrofit.create(Auth::class.java)
